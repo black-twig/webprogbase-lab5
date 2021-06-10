1e8dab1baf753a340f27bcf0b743e33804bf6fad
Olexandra Shestakova <oleksandra.shestakova@gmail.com>
2021-06-10 20:55:44 +0300

    config

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ const config = {
       port: process.env.APP_PORT || 3000
     },
     db: {
-      host: process.env.DB_HOST,
-      login: process.env.DB_LOGIN,
-      password: process.env.DB_PW,
-      name: process.env.DB_NAME
+      host: process.env.DB_HOST || 'lab4.0pfe1.mongodb.net',
+      login: process.env.DB_LOGIN || 'User123',
+      password: process.env.DB_PW || '123qweasd',
+      name: process.env.DB_NAME || 'Labs'
     }
    };
    
